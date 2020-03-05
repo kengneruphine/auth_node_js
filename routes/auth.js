466425f3be4e74f341c72cea320bd6cbb6c8bdb7
kengne ruphine <ruphinekengne@gmail.com>
2020-03-05 16:19:25 +0100

    hashing the user password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User')
+const bcrypt = require('bcryptjs');
 const {registerValidation, loginValidation} = require('../validation') 
 
 router.post('/register', async (req, res) => {
@@ -12,15 +13,19 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({ email: req.body.email });
     if (emailExist) return res.status(400).send('Email already exists');
 
+    //HASH THE PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     //create  a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user._id}); //returning only the user id
     } catch (err) {
         res.status(400).send(err);
     }
